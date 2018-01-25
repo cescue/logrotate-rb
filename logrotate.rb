a866068578218e8a2d90538e8def53c4cee76b87
Chris Escue <chris@escue.io>
2018-01-25 10:53:30 -0800

    Update regex to account for compression, improve readability

--- logrotate.rb
+++ logrotate.rb
@@ -13,9 +13,18 @@ config.merge!(DEFAULT_CONFIGURATION)
 
 abort('No directories specified. Nothing to do.') unless config['directories']
 
+pattern_to_rotate = %r{
+  ^
+  .+                                           # filename
+  \.(?:#{config['extensions'].join('|')})      # original file extension, eg .log
+  #{'(?:.zip)?' if config['compress'] == true} # .zip extention if applicable
+  (?:\.(\d+))?                                 # index after extension, eg .1
+  $
+}x
+
 config['directories'].each do |directory|
   Dir["#{directory}/*"].each do |entry|
-    next unless entry =~ /^.*\.(#{config['extensions'].join('|')})(?:\.\d+)?$/
+    next unless entry =~ pattern_to_rotate
 
   end
 end
