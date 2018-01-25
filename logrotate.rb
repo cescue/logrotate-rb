5baf37c9586a75633938629032c552ab1804d6c5
Chris Escue <chris@escue.io>
2018-01-25 10:31:45 -0800

    Load config file and iterate over specified directories

---
+++ logrotate.rb
@@ -0,0 +1,20 @@
+require 'fileutils'
+require 'yaml'
+
+DEFAULT_CONFIGURATION = {
+  'max_historic_files_per_log' => 10,
+  'compress' => true,
+  'extensions' => ['.log']
+}
+
+config = YAML.load_file('logrotaterb.yaml')
+
+config.merge!(DEFAULT_CONFIGURATION)
+
+abort('No directories specified. Nothing to do.') unless config['directories']
+
+config['directories'].each do |directory|
+  Dir[directory].each do |entry|
+
+  end
+end
